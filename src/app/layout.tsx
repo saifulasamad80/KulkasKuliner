ffb3dc7106c012794ff79d659965f4cb540940bc
saifulasamad80 <saifulasamad80@gmail.com>
2026-08-30 00:32:50 +0700

    fix: splash screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -11,6 +11,11 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+// INJEKSI WARNA TEMA HP (FOODDASH RED)
+export const viewport: Viewport = {
+  themeColor: '#DC2626',
+};
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://kulkaskuliner.vercel.app'),
@@ -48,7 +53,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="id" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased scroll-smooth`}>
-      <body className="min-h-full flex flex-col">
+      <body className="min-h-full flex flex-col bg-white">
         {children}
         
         <script
